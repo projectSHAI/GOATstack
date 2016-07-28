b2e780996b0b72256fe3deedacdddc3873e9b1ad
Chris <chris@discoveryourwonder.com>
2016-07-27 19:00:06 -0700

    added the mongodb to server.js and am trying to segregate it into the config directory

---
+++ config/env/dev.js
@@ -0,0 +1,12 @@
+MongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+  if (err) {
+    throw err;
+  }
+  db.collection('mammals').find().toArray(function(err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+    console.log(environment);
+  });
+});
--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@
 
 var express = require("express");
 var path = require("path");
+var MongoClient = require('mongodb').MongoClient;
 
 var port = process.env.PORT || 3000;
+var environment = process.env.NODE_ENV
+
 var app = express();
 
 app.use(express.static('client'));
@@ -18,3 +21,18 @@ var server = app.listen(port, function () {
     var port = server.address().port;
     console.log('This express app is listening on port:' + port);
 });
+
+if(environment === 'development') {
+  MongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+    if (err) {
+      throw err;
+    }
+    db.collection('mammals').find().toArray(function(err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log(result);
+      console.log(environment);
+    });
+  });
+}
