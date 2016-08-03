9c2a60c1ede39634de336e5e55024b35512e3660
Chris <chris@discoveryourwonder.com>
2016-08-02 19:48:02 -0700

    added wildcard routing to express so the server will serve the index file on any client query, then Angular will take care of routing once the index has been served

--- config/lib/express.js
+++ config/lib/express.js
@@ -6,9 +6,16 @@ var express = require('express'),
 module.exports.init = function () {
 	var app = express();
 
+	//exposes the client and node_modules folders to the client for file serving when client queries "/"
 	app.use(express.static('client'));
 	app.use(express.static('node_modules'));
-	app.get('/', function (req, res) {
+
+	//exposes the client and node_modules folders to the client for file serving when client queries anything, * is a wildcard
+	app.use('*', express.static('client'));
+	app.use('*', express.static('node_modules'));
+
+	//fire's a get function when any directory is queried (* is a wildcard) by the client, sends back the index.html as a response. Angular then does the proper routing on client side
+	app.get('*', function (req, res) {
 	    res.sendFile(path.resolve(__dirname, 'index.html'));
 	});
 
