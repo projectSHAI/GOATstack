87e49134a5d2815bf12cfee302b09df5d9970ddd
Chris <chris@discoveryourwonder.com>
2016-09-14 19:43:25 -0700

    added error handling and promises to the GET api request for the users api

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -13,7 +13,21 @@ var _ = require('lodash');
 var User = require('./user.model');
 
 module.exports.allUsers = function(req, res){	
-	res.json(User.find({}));
+		//User is a mongoose schema model
+		//.find is a mongoose method that accepts a json to search mongodb as the first argument, and a callback function that can take err and res as arguments	
+		User.find({})
+			//.then is a promise method that fires after the previous method and fires whatever function you place inside
+			//entity is the argument which catches the json object that is passed back from the mongodb query invoked by the .find method
+			.then(function(entity) {
+				//if we have the mongodb entity then we will send back the status 200 message and the json object 
+				if(entity)
+				res.status(200).json(entity)
+			})
+			//.catch will occur if one of the promises fails to do error handling
+			.catch(function(){
+				res.status(500).send({error: "Server did not get an entity response object from the database"})
+			});
+
 }
 
 module.exports.createUser = function(req, res){
