e2dd9748edf609e600fcea0695c302eef4baf7e2
JT <jc.thomas4214@gmail.com>
2016-10-04 01:25:08 -0700

    new wonder integration test

--- server/api/wonder/wonder.integration.js
+++ server/api/wonder/wonder.integration.js
@@ -9,6 +9,8 @@ describe('Wonder API:', function () {
   var newWonder;
   var wonders;
 
+  const inputs = [1, 43, 2, 35, 65, 36, 10, 57, 32, 45, 90, 79, 32, 47, 19, 14, 30, 15, 69, 25, 50, 38, 29];
+
   describe('GET /api/wonders', function () {
     beforeAll(function (done) {
       request(app)
@@ -30,49 +32,48 @@ describe('Wonder API:', function () {
   });
 
   describe('POST /api/wonders', function () {
-    var loop = function (counter) {
-      describe('wonder input', function () {
 
-        beforeAll(function (done) {
-          request(app)
-            .post('/api/wonders')
-            .send({
-              name: 'wonder: ' + counter % 8
-            })
-            .expect(200)
-            .expect('Content-Type', /json/)
-            .end((err, res) => {
-              if (err) {
-                done.fail(err);
-              }
-              newWonder = res.body;
-              done();
-            });
-        });
+    for (var counter = 0; counter < inputs.length; counter++) {
+      (function (input, output, counter) {
+        describe('wonder input', () => {
 
-        beforeAll(function (done) {
-          request(app)
-            .get('/api/wonders')
-            .expect(200)
-            .expect('Content-Type', /json/)
-            .end((err, res) => {
-              if (err) {
-                done.fail(err);
-              }
-              wonders = res.body;
-              done();
-            });
-        });
+          beforeAll((done) => {
+            request(app)
+              .post('/api/wonders')
+              .send({
+                name: 'wonder: ' + inputs[counter]
+              })
+              .expect(200)
+              .expect('Content-Type', /json/)
+              .end((err, res) => {
+                if (err) {
+                  done.fail(err);
+                }
+                newWonder = res.body;
+                done();
+              });
+          });
 
+          beforeAll((done) => {
+            request(app)
+              .get('/api/wonders')
+              .expect(200)
+              .expect('Content-Type', /json/)
+              .end((err, res) => {
+                if (err) {
+                  done.fail(err);
+                }
+                wonders = res.body;
+                done();
+              });
+          });
 
-        it('should respond with the correct wonder: ' + counter % 8 + ' at index: ' + counter % 20, function () {
-          expect(wonders[counter % 20].name).toEqual('wonder: ' + counter % 8);
-        });
-      });
-    };
-
-    for (var i = 0; i < 23; i++) {
-      loop(i);
+          it('should respond with the correct wonder: ' + input + ' at index: ' + output, () => {
+            expect(wonders[output].name).toEqual('wonder: ' + input);
+          });
+        })
+      })(inputs[counter], counter % 20, counter)
     }
+
   });
 });
