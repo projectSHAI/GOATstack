d7db1f0744b0791f414bbf66c1056deddbea722b
JT <jc.thomas4214@gmail.com>
2016-10-03 22:56:18 -0700

    karma reporter

--- config/sys/karma.conf.js
+++ config/sys/karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-mocha-reporter'),
       require('karma-jasmine-html-reporter'), // click "Debug" in browser to see it
       require('karma-htmlfile-reporter') // crashing w/ strange socket error
     ],
@@ -84,7 +85,7 @@ module.exports = function(config) {
     exclude: [],
     preprocessors: {},
     // disabled HtmlReporter; suddenly crashing w/ strange socket error
-    reporters: ['progress', 'kjhtml'],//'html'],
+    reporters: ['mocha'],//'html'],
 
     // HtmlReporter configuration
     htmlReporter: {
