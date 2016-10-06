80639448c0a9dd9d92c1908c4fd017e604d8e6e8
JT <jc.thomas4214@gmail.com>
2016-10-05 18:22:22 -0700

    condensed server side wonders test

--- server/api/wonder/wonder.integration.js
+++ server/api/wonder/wonder.integration.js
@@ -11,6 +11,33 @@ describe('Wonder API:', function () {
 
   const inputs = [1, 43, 2, 35, 65, 36, 10, 57, 32, 45, 90, 79, 32, 47, 19, 14, 30, 15, 69, 25, 50, 38, 29];
 
+  describe('POST /api/wonders', function () {
+    for (var counter = 0; counter < inputs.length; counter++) {
+      (function (input) {
+        return beforeAll((done) => {
+          request(app)
+            .post('/api/wonders')
+            .send({
+              name: 'wonder: ' + input
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+              if (err) {
+                done.fail(err);
+              }
+              expect(res.body.name).toEqual('wonder: ' + input);
+              done();
+            });
+        });
+      })(inputs[counter]);
+    }
+
+    it('should respond back each query with inputted wonder', () => {
+      expect(true).toEqual(true);
+    });
+  });
+
   describe('GET /api/wonders', function () {
     beforeAll(function (done) {
       request(app)
@@ -29,63 +56,13 @@ describe('Wonder API:', function () {
     it('should respond with JSON array', function () {
       expect(wonders).toEqual(jasmine.any(Array));
     });
-  });
 
-  describe('POST /api/wonders', function () {
-
-    describe('Input Wonders', () => {
-      for (var counter = 0; counter < inputs.length; counter++) {
-        (function (input) {
-          it('should respond back with inputted wonder: ' + input, (done) => {
-            request(app)
-              .post('/api/wonders')
-              .send({
-                name: 'wonder: ' + input
-              })
-              .expect(200)
-              .expect('Content-Type', /json/)
-              .end((err, res) => {
-                if (err) {
-                  done.fail(err);
-                }
-                expect(res.body.name).toEqual('wonder: ' + input);
-                done();
-              });
-          });
-        })(inputs[counter]);
-      }
-    });
-
-    describe('Gather Wonders Array', () => {
-      it('should respond back with all current wonders', (done) => {
-        request(app)
-          .get('/api/wonders')
-          .expect(200)
-          .expect('Content-Type', /json/)
-          .end((err, res) => {
-            if (err) {
-              done.fail(err);
-            }
-            wonders = res.body;
-
-            expect(wonders).toEqual(jasmine.any(Array));
-            expect(wonders.length).toEqual(20);
-
-            done();
-          });
-      });
-    });
-
-    describe('Check Wonders Response Array', () => {
+    it('wonders should equal the original input array', () => {
       for (var i = 0; i < 20; i++) {
         (function (input, counter) {
-          it('wonders[' + counter + '] should equal wonder: ' + input, (done) => {
-            expect(wonders[counter].name).toEqual('wonder: ' + input)
-            done();
-          });
+          return expect(wonders[counter].name).toEqual('wonder: ' + input)
         })(inputs[i + 3], (i + 3) % 20)
       }
     });
-
   });
 });
