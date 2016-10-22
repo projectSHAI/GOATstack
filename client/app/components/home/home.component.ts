c680e177e8b444a8837b57927e9bea836af0d769
JT <jc.thomas4214@gmail.com>
2016-10-21 17:43:52 -0700

    rebase fix

--- client/app/components/home/home.component.ts
+++ client/app/components/home/home.component.ts
@@ -1,19 +1,7 @@
 import { Component } from '@angular/core';
 
-import { WonderService } from '../../services/wonder/wonder.service';
-import { SocketService } from '../../services/socketio/socketio.service';
-
-import { Wonder } from '../../models/wonder/wonder.model';
-
-//give javascript variables from custom js libraries a type so the
-//typescript compiler does not throw an error TS2304: Cannot find name 'var'.
-declare let TweenMax: any;
-declare let TimelineMax: any;
-
 @Component({
-  // moduleId: module.id,
   selector: 'home-section',
-  providers: [WonderService, SocketService],
   template: `
     <sun-and-moon [style.left.%]="sunXPos" [style.top.%]="sunYPos" ></sun-and-moon>
     <cloud-generator></cloud-generator>
@@ -50,49 +38,7 @@ declare let TimelineMax: any;
 })
 
 export class HomeComponent {
-  wonders: Wonder[];
-  socket;
 
-  sunXPos: number = 24;
-  sunYPos = 1;
-
-  constructor(private wonderService: WonderService) {
-    this.socket = new SocketService();
-  }
-
-  ngOnInit() {
-    this.wonderService.getWonders()
-      .subscribe(wonders => {
-        this.wonders = wonders;
-        this.socket.syncUpdates('Wonder', this.wonders);
-      });
-
-    let kiwi = document.getElementById('hello');
-
-
-    let tl = new TimelineMax();
-
-    tl.to(kiwi, 1, { x: 50 }).to(kiwi, 1, { y: 50 }).to(kiwi, 1, { opacity: 0.5 });
-
-  }
-
-  ngOnDestroy() {
-    this.socket.unsyncUpdates('Wonder');
-  }
-
-  saveWonder(name: string) {
-    this.wonderService.saveWonder(name)
-      .subscribe(() => {
-        // console.log('saveWonder returns');
-      });
-  }
-
-  cloudAnim() {
-    let kiwi = document.getElementById('hello');
-
-    let tl = new TimelineMax();
-
-    tl.to(kiwi, 1, {x: 50}).to(kiwi, 1, {y: 50}).to(kiwi, 1, {opacity: 0.5});
-  }
+  constructor() { }
 
 }
