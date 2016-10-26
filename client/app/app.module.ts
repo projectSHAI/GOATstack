5a1972d46fce7606f2007f7a5bb98d72eab3191d
CHRISTOPHER HAUGEN <CHAUGEN@fll.ad.emb>
2016-10-26 16:20:53 -0400

    added documentation and comments for organization best practices in app.module.ts

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,22 +1,53 @@
-///////////////
-//Root module//
-///////////////
+/*
+==============================================================
+* Root Module
+==============================================================
+// Any assets included in this file will be attached
+// to the global scope of the application.
+*/
 
-//main component to bootstrap too
+/*
+--------------------------------------------------
+Main component which gets bootstrapped
+--------------------------------------------------
+//Named AppComponent in compliance with Angular best practices
+*/
 import { AppComponent }  from './components/app/app.component';
 
-import { Http, XHRBackend, RequestOptions } from '@angular/http';
-
+/*
+--------------------------------------------------
+Modules
+--------------------------------------------------
 //other necessary modules for this app
+*/
 import { NgModule }                  from '@angular/core';
 import { FormsModule }               from '@angular/forms';
 import { BrowserModule }             from '@angular/platform-browser';
 import { HttpModule, JsonpModule }   from '@angular/http';
 
-//routing imports
+/*
+--------------------------------------------------
+Routing
+--------------------------------------------------
+//enables navigation capabilities capitilizing on the browsers history stack
+*/
 import { routing } from './routes';
 
-//components for different views
+/*
+--------------------------------------------------
+HTTP Requests
+--------------------------------------------------
+//imports to handle http events to send and receive data from api's
+*/
+import { Http, XHRBackend, RequestOptions } from '@angular/http';
+
+/*
+--------------------------------------------------
+Components
+--------------------------------------------------
+//Declare components here
+*/
+//user created components
 import { HeaderComponent }            from './components/header/header.component';
 import { NavbarComponent }            from './components/navbar/navbar.component';
 import { SignInOutComponent }         from './components/signinout/signinout.component';
@@ -29,22 +60,62 @@ import { MountainRangeComponent }     from './components/mountain-range/mountain
 import { GOATComponent }              from './components/GOAT/GOAT.component';
 import { CloudGeneratorComponent }    from './components/cloud-generator/cloud-generator.component';
 
-//services that need to be singletons
+//Angular and 3rd party components
+
+/*
+--------------------------------------------------
+Directives
+--------------------------------------------------
+//Declare directives here
+*/
+//user created directives
+
+//Angular and 3rd party directives
+
+/*
+--------------------------------------------------
+Services
+--------------------------------------------------
+//Declare services that need to be singletons here
 // import { SocketService }              from './services/socketio/socketio.service';
+*/
+//user created services
 import { ErrorHandlerService }        from './services/errorHandler/errorHandler.service';
 
-//custom pipes
-import { NgForHookPipe }              from './pipes/ngFor-hook.pipe';
-
-//services for global use
+//Angular and 3rd party serices
 import { Cookie }                     from 'ng2-cookies/ng2-cookies';
 import { MaterialModule }             from '@angular/material';
 import { HttpIntercept }              from './services/auth/auth.service';
 
-//declare all custom non npm libraries here
+/*
+--------------------------------------------------
+Pipes
+--------------------------------------------------
+//Declare pipes here
+*/
+//User created pipes
+import { NgForHookPipe }              from './pipes/ngFor-hook.pipe';
+
+//Angular and 3rd party pipes
+
+/*
+--------------------------------------------------
+Non NPM libraries
+--------------------------------------------------
+//Declare all custom non npm libraries here
+//There are thousands of non NPM javascript libraries, why limit ourselves!!
+// ----- Note if possible also include a types definition file.
+*/
 import 'gsap';
 
-//decorator which packages all resources for the app
+
+/*
+--------------------------------------------------
+NgModule
+--------------------------------------------------
+//decorator which packages all resources imported above for the app
+//without this decorator Angular cannot use any of those above assets
+*/
 @NgModule({
   //imports: this object imports helper modules which are children in the module tree
   imports: [
