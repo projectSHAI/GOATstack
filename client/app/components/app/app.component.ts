29d547c376039f87356894c7dc95b49887ac13bd
CHRISTOPHER HAUGEN <CHAUGEN@fll.ad.emb>
2016-10-31 10:36:39 -0400

    Added docs to app.component files

--- client/app/components/app/app.component.ts
+++ client/app/components/app/app.component.ts
@@ -1,18 +1,36 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+/*
+=========================================================================================================
+Bootstrapping component
+=========================================================================================================
+//According to Angular best practices the App component should be used for bootstrapping the application.
+//This component gets bootstrapped through app.module.ts, the magic occurs in the @NgModule decorater's bootstrap property,
+//we set that value to the AppComponent class defined in this component
+//then the app.module.ts gets invoked in the main.ts bootstrap method.
+*/
+
+
+//main imports
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 import { NgRedux, select } from 'ng2-redux';
 import { ErrorHandlerActions } from '../../actions/errorHandler.actions';
 import { Observable } from 'rxjs/Observable';
 
+//decorator
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
+//class which is implemented once the AfterViewInit event in tha Angular event lifecycle has fired.
+//-- to learn more about Angular's event lifecycle read here: https://angular.io/docs/ts/latest/guide/lifecycle-hooks.html
 export class AppComponent implements AfterViewInit {
+  //this decorator is for NgRedux. you can read more about Redux here: https://github.com/angular-redux/ng2-redux
   @select('error') error$: Observable<string>;
 
-  @ViewChild('errorToast') errorToast;
+  //this decorator gabs the object associated with the #errorToast template variable assigned in the app.componnent.html file,
+  //-- and assigns this object to the class variable errorToast
+  @ViewChild('errorToast') errorToast: ElementRef;
 
   constructor(private errorHandler: ErrorHandlerActions) { }
 
--- client/app/services/wonder/wonder.service.ts
+++ client/app/services/wonder/wonder.service.ts
@@ -15,7 +15,6 @@ export class WonderService {
     // In a real world app, we might use a remote logging infrastructure
     // We'd also dig deeper into the error to get a better message
     let body = JSON.parse(error._body);
-    
     let errMsg = (body.message) ? body.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
     return Observable.throw(errMsg);
