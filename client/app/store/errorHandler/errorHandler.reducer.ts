d1b4a65060a99e3276f67486f350c16dea13678a
JT <jc.thomas4214@gmail.com>
2016-11-01 23:55:12 -0700

    errorHandlerReducer.spec tests

--- client/app/store/errorHandler/errorHandler.reducer.ts
+++ client/app/store/errorHandler/errorHandler.reducer.ts
@@ -7,8 +7,9 @@ import { INITIAL_STATE } from './errorHandler.initial-state';
 export function errorHandlerReducer(state: IError = INITIAL_STATE, action: any) {
   switch(action.type) {
     case ErrorHandlerActions.SHOW_ERROR:
-    case ErrorHandlerActions.HIDE_ERROR:
       return state.updateIn(['message'], val => action.payload);
+    case ErrorHandlerActions.HIDE_ERROR:
+      return state.updateIn(['message'], val => '');
     default:
       return state;
   }
--- client/app/store/errorHandler/errorHandler.spec.ts
+++ client/app/store/errorHandler/errorHandler.spec.ts
@@ -13,4 +13,22 @@ describe('ErrorHandler Reducer', () => {
   it('should have an immutable initial state', () => {
     expect(Map.isMap(initialState)).toBe(true);
   });
+
+  it('should set the error message on SHOW_ERROR', () => {
+    const previousValue = initialState.getIn(['message']);
+    const nextState = errorHandlerReducer(initialState,
+      { type: ErrorHandlerActions.SHOW_ERROR, payload: 'Testing Error Message' });
+    expect(nextState.getIn(['message'])).toBe('Testing Error Message');
+  });
+
+  it('should set the error message on HIDE_ERROR', () => {
+    // First SHOW_ERROR and check
+    const nextState = errorHandlerReducer(initialState,
+      { type: ErrorHandlerActions.SHOW_ERROR, payload: 'Testing Error Message' });
+    expect(nextState.getIn(['message'])).toBe('Testing Error Message');
+    // Then HIDE_ERROR and check
+    const nextState2 = errorHandlerReducer(nextState,
+      { type: ErrorHandlerActions.HIDE_ERROR });
+    expect(nextState2.getIn(['message'])).toBe('');
+  });
 });
