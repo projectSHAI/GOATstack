7b9ed8fe7c28670f8f82dec783ee9137e5a47d42
JT <jc.thomas4214@gmail.com>
2016-11-02 23:47:09 -0700

    cloud actions and reducer tests

--- client/app/actions/cloud/cloud.actions.ts
+++ client/app/actions/cloud/cloud.actions.ts
@@ -1,105 +1,21 @@
-import { Injectable, ElementRef } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import { NgRedux, select } from 'ng2-redux';
+import { NgRedux } from 'ng2-redux';
 import { IAppState } from '../../store';
-
-import { WonderActions } from '../wonder/wonder.actions';
-
-declare let TweenMax: any;
-declare let TimelineMax: any;
-declare let Power0: any;
 
 @Injectable()
 export class CloudActions {
-  @select('animaArray') animaArray$: Observable<any>;
-  private animaArray: any;
-  private width: number;
-
-  constructor(private ngRedux: NgRedux<IAppState>) {
-    this.width = window.innerWidth;
-    this.animaArray$.subscribe(anima => this.animaArray = anima);
-  }
+  constructor(private ngRedux: NgRedux<IAppState>) { }
 
   static CHANGE_STYLES: string = 'CHANGE_STYLES';
   static CHANGE_ANIMA: string = 'CHANGE_ANIMA';
 
-  private loopAnima(index: number): void {
-    this.animaArray.get(index).restart();
-  }
-
-  cloudType(wonderLength: number, index: number, killTime: boolean = false): void {
-    let randomInt = this.rndInt(1, 3);
-
-    if (wonderLength <= 4) {
-
-      switch (randomInt) {
-        case 1:
-          this.ngRedux.dispatch({ type: CloudActions.CHANGE_STYLES, payload: { index: index, asset: 'smallcloud1' } });
-          break;
-        case 2:
-          this.ngRedux.dispatch({ type: CloudActions.CHANGE_STYLES, payload: { index: index, asset: 'smallcloud2' } });
-          break;
-        case 3:
-          this.ngRedux.dispatch({ type: CloudActions.CHANGE_STYLES, payload: { index: index, asset: 'smallcloud3' } });
-          break;
-      }
-
-    }
-    else if (wonderLength > 4 && wonderLength <= 15) {
-
-      switch (randomInt) {
-        case 1:
-          this.ngRedux.dispatch({ type: CloudActions.CHANGE_STYLES, payload: { index: index, asset: 'mediumcloud1' } });
-          break;
-        case 2:
-          this.ngRedux.dispatch({ type: CloudActions.CHANGE_STYLES, payload: { index: index, asset: 'mediumcloud2' } });
-          break;
-        case 3:
-          this.ngRedux.dispatch({ type: CloudActions.CHANGE_STYLES, payload: { index: index, asset: 'mediumcloud3' } });
-          break;
-      }
-
-    }
-    else {
-
-      switch (randomInt) {
-        case 1:
-          this.ngRedux.dispatch({ type: CloudActions.CHANGE_STYLES, payload: { index: index, asset: 'largecloud1' } });
-          break;
-        case 2:
-          this.ngRedux.dispatch({ type: CloudActions.CHANGE_STYLES, payload: { index: index, asset: 'largecloud2' } });
-          break;
-        case 3:
-          this.ngRedux.dispatch({ type: CloudActions.CHANGE_STYLES, payload: { index: index, asset: 'largecloud3' } });
-          break;
-      }
-
-    }
-
-  }
-
-  cloudAnima(value: string, el: ElementRef, object: any, index: number): string {
-    if (this.animaArray.size === 10)
-      this.animaArray.get(index).kill();
-
-    const anima = new TimelineMax({
-      callbackScope: this,
-      onComplete: this.loopAnima,
-      onCompleteParams: [index]
-    });
-
-    // TODO: find a way to get the initial element position to subtract from innerWidth
-    anima
-      .to(el, 0, { ease: Power0.easeNone, left: '-350px', x: '0', y: '0' })
-      .to(el, this.rndInt(30, 85), { ease: Power0.easeNone, x: 1707 + 350, y: this.rndInt(-200, 200) });
-
-    // Push new gsap timeline to animaArray List
+  changeAnima(anima: any, index: number) {
     this.ngRedux.dispatch({ type: CloudActions.CHANGE_ANIMA, payload: { index: index, timeline: anima } });
-    return value;
   }
 
-  rndInt(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  changeStyle(asset: string, index: number) {
+    this.ngRedux.dispatch({ type: CloudActions.CHANGE_STYLES, payload: { index: index, asset: asset } });
   }
 }
--- client/app/components/cloud-generator/cloud-generator.component.ts
+++ client/app/components/cloud-generator/cloud-generator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { select } from 'ng2-redux';
 
@@ -9,6 +9,8 @@ import { SocketService } from '../../services/socketio/socketio.service';
 import { CloudActions } from '../../actions/cloud/cloud.actions';
 
 declare let TweenMax: any;
+declare let TimelineMax: any;
+declare let Power0: any;
 
 @Component({
   selector: 'cloud-generator',
@@ -19,7 +21,10 @@ declare let TweenMax: any;
 
 export class CloudGeneratorComponent {
   @select('cloudStyle') cloudStyle$: Observable<any>;
+  @select('animaArray') animaArray$: Observable<any>;
   @select('wonder') wonder$: Observable<any>;
+  private animaArray: any;
+  private width: number;
 
   @ViewChild('wonderSky') wonderSky;
 
@@ -31,20 +36,22 @@ export class CloudGeneratorComponent {
     private clockService: ClockService) { }
 
   ngOnInit() {
+    this.width = window.innerWidth;
     this.clockService.currentTime.subscribe(time => this.timeOfDayCss());
+    this.animaArray$.subscribe(anima => this.animaArray = anima);
     this.wonderService.getWonders()
       .subscribe(wonders => {
         // initialize store wonders
         this.wonderActions.initWonders(wonders);
         // initialize store cloudStyle
-        wonders.forEach((item, index) => this.cloudActions.cloudType(item.name.length, index));
+        wonders.forEach((item, index) => this.cloudType(item.name.length, index));
         // initialize socketio listener with backCall and delay till callback
         this.socket.syncUpdates('Wonder', wonders, ['CHANGE_WONDERS'], null, (item, index) => {
 
           TweenMax.to(this.wonderSky.nativeElement.children[index], 1, {
             opacity: 0,
-            callbackScope: this.cloudActions,
-            onComplete: this.cloudActions.cloudType,
+            callbackScope: this,
+            onComplete: this.cloudType,
             onCompleteParams: [item.name.length, index, true]
           });
 
@@ -55,6 +62,78 @@ export class CloudGeneratorComponent {
   ngOnDestroy() {
     // detach socket listening when component is destroyed
     this.socket.unsyncUpdates('Wonder');
+  }
+
+  private loopAnima(index: number): void {
+    this.animaArray.get(index).restart();
+  }
+
+  cloudAnima(value: string, el: ElementRef, object: any, index: number): string {
+    if (this.animaArray.size === 10)
+      this.animaArray.get(index).kill();
+
+    const anima = new TimelineMax({
+      callbackScope: this,
+      onComplete: this.loopAnima,
+      onCompleteParams: [index]
+    });
+
+    // TODO: find a way to get the initial element position to subtract from innerWidth
+    anima.to(el, 0, { ease: Power0.easeNone, left: '-350px', x: '0', y: '0' })
+      .to(el, this.rndInt(30, 85), { ease: Power0.easeNone, x: this.width + 350, y: this.rndInt(-200, 200) });
+
+    // Push new gsap timeline to animaArray List
+    this.cloudActions.changeAnima(anima, index);
+    return value;
+  }
+
+  cloudType(wonderLength: number, index: number): void {
+    let randomInt = this.rndInt(1, 3);
+
+    if (wonderLength <= 4) {
+      switch (randomInt) {
+        case 1:
+          this.cloudActions.changeStyle('smallcloud1', index);
+          break;
+        case 2:
+          this.cloudActions.changeStyle('smallcloud2', index);
+          break;
+        case 3:
+          this.cloudActions.changeStyle('smallcloud3', index);
+          break;
+      }
+    }
+    else if (wonderLength > 4 && wonderLength <= 15) {
+      switch (randomInt) {
+        case 1:
+          this.cloudActions.changeStyle('mediumcloud1', index);
+          break;
+        case 2:
+          this.cloudActions.changeStyle('mediumcloud2', index);
+          break;
+        case 3:
+          this.cloudActions.changeStyle('mediumcloud3', index);
+          break;
+      }
+    }
+    else {
+      switch (randomInt) {
+        case 1:
+          this.cloudActions.changeStyle('largecloud1', index);
+          break;
+        case 2:
+          this.cloudActions.changeStyle('largecloud2', index);
+          break;
+        case 3:
+          this.cloudActions.changeStyle('largecloud3', index);
+          break;
+      }
+    }
+
+  }
+
+  rndInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   timeOfDayCss() {
@@ -68,8 +147,6 @@ export class CloudGeneratorComponent {
       this.wonderSky.nativeElement.style.filter = "brightness(70%)";
     }
     else if(this.clockService.nightTime) {
-      console.log(this.wonderSky.nativeElement.children.length);
-
       this.wonderSky.nativeElement.style.filter = "brightness(30%)";
     }
     else{
