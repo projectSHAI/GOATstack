20ec5c12e553c84ec8f025f46148105e2b301b42
JT <jc.thomas4214@gmail.com>
2016-11-07 20:59:49 -0800

    cloud now move at constant speed

--- app/components/cloud-generator/cloud-generator.component.ts
+++ app/components/cloud-generator/cloud-generator.component.ts
@@ -58,7 +58,7 @@ export class CloudGeneratorComponent implements OnInit, OnDestroy {
             onCompleteParams: [item.name.length, index, true]
           });
 
-        }, 1000);
+        }, 1250); // Give a little more time to render the new cloud style
       });
   }
 
@@ -68,22 +68,47 @@ export class CloudGeneratorComponent implements OnInit, OnDestroy {
   }
 
   private loopAnima(index: number): void {
-    this.animaArray.get(index).restart();
+    this.animaArray.get(index).play('loop');
+  }
+
+  private speed(pos: number, min: number, max: number, factor: number): number {
+    // find the speed by dividing the percentage left till off the screen
+    // by the amount of %/second you want all clound to travel at
+    // and you will get the second necessary to travel the remaining
+    // distance. Add a random number to get a virtual speed diff 
+    const speed = (100 - pos + this.rndInt(min, max))/factor;
+    // make sure the time doesn't fall bellow a minimum threshold
+    return speed > 8 ? speed : 8;
   }
 
   cloudAnima(value: string, el: ElementRef, object: any, index: number): string {
+    // This function will be called every time a new wonder arrives
+    // after initial start of the app this should always fire
     if (this.animaArray.size === 10)
       this.animaArray.get(index).kill();
+    // the travel finds the amount of pixels left the cloud
+    // must travel to get to the end of the screen from the % xcoor
+    const travel = this.width - (this.width * (object.xcoor/100));
+    // Since we want all the cloud to move accross the screen at the
+    // same speed we need a constant factor to divide the delta
+    const factor = 2.5;
+    // rf values are for random factor to simulate speed difference
+    // relative to the constant speed
+    const rfMin = -15;
+    const rfMax = 15;
 
+    // Create the gsap timeline to loop
     const anima = new TimelineMax({
       callbackScope: this,
       onComplete: this.loopAnima,
       onCompleteParams: [index]
     });
 
-    // TODO: find a way to get the initial element position to subtract from innerWidth
-    anima.to(el, 0, { ease: Power0.easeNone, left: '-350px', x: '0', y: '0' })
-      .to(el, this.rndInt(30, 85), { ease: Power0.easeNone, x: this.width + 350, y: this.rndInt(-200, 200) });
+    anima.to(el, this.rndInt(1,3), { opacity: 1 })
+      .to(el, this.speed(object.xcoor,rfMin,rfMax,factor), { ease: Power0.easeNone, x: travel, y: this.rndInt(-100, 100) }, 0)
+      .addLabel('loop', '+=0')
+      .to(el, 0, { ease: Power0.easeNone, left: '-350px', x: '0', y: '0' })
+      .to(el, this.speed(-10,rfMin,rfMax,factor), { ease: Power0.easeNone, x: this.width + 350, y: this.rndInt(-100, 100) });
 
     // Push new gsap timeline to animaArray List
     this.cloudActions.changeAnima(anima, index);
