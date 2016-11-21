bdca2cde32edf4cb7c624ac2ca269363ff7ccb56
Chris <chris@discoveryourwonder.com>
2016-11-20 18:26:15 -0800

    refactored zoom animation, it is finally smooth

--- app/components/island/island.component.ts
+++ app/components/island/island.component.ts
@@ -4,6 +4,8 @@ import { select } from 'ng2-redux';
 import { Observable } from 'rxjs/Observable';
 
 import { WonderActions } from '../../actions/wonder/wonder.actions';
+import { ZoomActions } from '../../actions/zoom/zoom.actions';
+import { ZoomDirective } from '../../directives/zoom.directive';
 
 @Component({
   selector: 'the-island',
@@ -15,15 +17,25 @@ import { WonderActions } from '../../actions/wonder/wonder.actions';
 export class IslandComponent implements OnInit {
 
   @select('timeOfDay') toda$: Observable<any>;
+  @select('zoom') zoom$: Observable<any>;
 
   dream: string;
   islandSvg: string;
+  showInput: boolean;
 
-  constructor(public wonderActions: WonderActions){ }
+  constructor(
+    public wonderActions: WonderActions,
+    public zoomActions: ZoomActions,
+    public zoomDirective: ZoomDirective
+    ){ }
 
   ngOnInit() {
     this.toda$.subscribe(x => {
       this.islandSvg = x.get('islandSvg');
+    });
+
+    this.zoom$.subscribe(x => {
+      this.showInput = x.get('showHide');
     });
   }
 
--- app/directives/zoom.directive.ts
+++ app/directives/zoom.directive.ts
@@ -1,52 +1,84 @@
 import { Directive, ElementRef, Input, Renderer, HostListener, OnInit } from '@angular/core';
+import { ZoomActions } from '../actions/zoom/zoom.actions';
 
 declare let TweenMax: any;
 declare let TimelineMax: any;
 declare let Power0: any;
 
 @Directive({ 
-	selector: '[zoomNotScroll]' 
+	selector: '[zoomNotScroll]',
+    providers: [ZoomActions] 
 })
 
 export class ZoomDirective {
 
-    windowWidth: number;
+    windowWidth: number = window.innerWidth;
     windowHeight: number = window.innerHeight;
-    scrollTop: number;
+    scrollTop: number = document.body.scrollTop;
+    prevScrollTop: number = this.scrollTop;
 
     //expected screen ratio greater than 1 = landscape less than 1 equals portrait
-    aspectRatio: number;
+    aspectRatio: number = this.windowWidth/this.windowHeight;
 
 
-    eleNode: any = this.el.nativeElement;
+    oceanNode: any = this.el.nativeElement;
+    homeNode: any = this.oceanNode.parentNode;
     landscapeTl: any = new TimelineMax({paused: true});
     portraitTl: any = new TimelineMax({paused: true});
 
-    constructor(private el: ElementRef, private renderer: Renderer) { 
+    constructor(
+        private el: ElementRef, 
+        private renderer: Renderer,
+        private zoomActions: ZoomActions) { 
         
     }
 
     ngOnInit() {
-        this.landscapeTl.to(this.eleNode, 1, {scale: 1, x: 0, y: this.windowHeight / 2});
+        this.landscapeTl.to(this.oceanNode, 1, {scale: 1, x: 0, y: this.windowHeight / 2}).to(this.homeNode, 0, {height: 'auto'}, 0);
 
-        this.portraitTl.to(this.eleNode, 1, {scale: 1, x: 0, y: this.windowHeight / 2});
+        this.portraitTl.to(this.oceanNode, 1, {scale: 1, x: 0, y: this.windowHeight / 2}).to(this.homeNode, 0, {height: 'auto'}, 0);
     }
-
-    @HostListener('window:resize', ['$event'])  
+  
     @HostListener('window:scroll', ['$event'])
     scroll(event) {
+        this.prevScrollTop = this.scrollTop;
+        this.windowWidth = window.innerWidth;
+        this.windowHeight = window.innerHeight;
+        this.scrollTop = document.body.scrollTop;
 
-    this.windowWidth = window.innerWidth;
-    this.windowHeight = window.innerHeight;
-    this.scrollTop = document.body.scrollTop;
-    this.aspectRatio = this.windowWidth/this.windowHeight;
+        if(this.scrollTop === 0 && this.aspectRatio > 1) {
+            this.landscapeTl.reverse(0);
+            this.zoomActions.updateShowHide();
+        }
+        else if(this.scrollTop === 0 && this.aspectRatio < 1) {
+            this.portraitTl.reverse(0);
+            this.zoomActions.updateShowHide(); 
+        }
 
-    if(this.aspectRatio > 1) { 
-        this.scrollTop !== 0 ? this.landscapeTl.play() : this.landscapeTl.reverse(0);
-    } else {
-        this.scrollTop !== 0 ? this.portraitTl.play() : this.portraitTl.reverse(0);
     }
-    
+
+    @HostListener('window:resize', ['$event'])
+    resize(event) {
+        this.windowWidth = window.innerWidth;
+        this.windowHeight = window.innerHeight;
+        this.aspectRatio = this.windowWidth/this.windowHeight;
+
+        this.landscapeTl.to(this.oceanNode, 1, {scale: 1, x: 0, y: this.windowHeight / 2}).to(this.homeNode, 0, {height: 'auto'});
+
+        this.portraitTl.to(this.oceanNode, 1, {scale: 1, x: 0, y: this.windowHeight / 2}).to(this.homeNode, 0, {height: 'auto'});
+
+    }
+
+    zoomOut() {
+        console.log(this.homeNode);
+        if(this.aspectRatio > 1) {
+            this.landscapeTl.play(0);
+            this.zoomActions.updateShowHide();
+        }
+        else if(this.aspectRatio < 1) {
+            this.portraitTl.play(0);
+            this.zoomActions.updateShowHide(); 
+        }
     }
 
 }
