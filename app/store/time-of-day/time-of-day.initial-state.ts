22b09a605bf2e69cb46a2ffce48ed076a513264d
JT <jc.thomas4214@gmail.com>
2016-11-20 20:30:36 -0800

    time of day initial-state changed to night

--- app/store/time-of-day/time-of-day.initial-state.ts
+++ app/store/time-of-day/time-of-day.initial-state.ts
@@ -2,12 +2,12 @@ import { reimmutifyTimeOfDay } from './time-of-day.transformers';
 
 // Define the TIME_OF_DAY for the cloudStyle Object
 export const INITIAL_STATE = reimmutifyTimeOfDay({
-    cloudBrightness: 'brightness(100%)',
-    skySvg: 'assets/day-sky.svg',
-    mountainGoatSvg: 'assets/day-mountain-goat.svg',
-    islandSvg: 'assets/day-island.svg',
+    cloudBrightness: 'brightness(30%)',
+    skySvg: 'assets/night-sky.svg',
+    mountainGoatSvg: 'assets/night-mountain-goat.svg',
+    islandSvg: 'assets/night-island.svg',
     oceanFrontSvg: 'assets/day-ocean.svg',
-    whaleSvg: 'assets/day-whale.svg',
-    sunMoonGlow: '0px 0px 100px 12px yellow',
-    sunMoonBorder: 'rgba(255,255,0, 0.3)'
+    whaleSvg: 'assets/night-whale.svg',
+    sunMoonGlow: '0px 0px 100px 12px purple',
+    sunMoonBorder: 'rgba(102,51,153, 0.3)'
 });
