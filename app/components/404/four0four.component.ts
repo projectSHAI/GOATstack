19aa5d1443e5d59a42353de174e2f4227ee03c7c
Chris <chris@discoveryourwonder.com>
2016-12-03 14:39:42 -0800

    changed 404 page

--- app/components/404/four0four.component.ts
+++ app/components/404/four0four.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'four0four-section',
-	template: `<img src="assets/pedobear.jpg">`
+	template: `<p>404 Page not found</p>`,
+	styles: [`
+		p {
+		    font-size: 24px;
+		    display: block;
+		    position: relative;
+		    width: 100%;
+		    text-align: center;
+		    margin-top: 100px;
+		}
+	`]
 })
 
 export class Four0FourComponent { }
