09775a2698aaa8ff2235c2b7f6e65384144ac367
Chris <chaugen24@knights.ucf.edu>
2017-01-07 15:29:25 -0800

    added epipelagic caps and proper placement for them

--- client/ocean-segment/components/epipelagic-zone/epipelagic-zone.component.ts
+++ client/ocean-segment/components/epipelagic-zone/epipelagic-zone.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, HostListener } from '@angular/core';
 
 import { select } from 'ng2-redux';
 import { Observable } from 'rxjs/Observable';
@@ -11,8 +11,11 @@ import { Observable } from 'rxjs/Observable';
 
 export class EpipelagicZoneComponent implements OnInit{ 
 
+	@ViewChild('oceanCap') oceanCap: ElementRef;
 	@select('timeOfDay') toda$: Observable<any>;
+	@select('zoom') zoom$: Observable<any>;
 
+	overlayMargin: string;
 	epipelagicCapOverlaySvg: string;
 	epipelagicColor: string = 'rgba(19,15,39,0.4)';;
 
@@ -21,7 +24,7 @@ export class EpipelagicZoneComponent implements OnInit{
 	ngOnInit() {
 
 		this.toda$.subscribe((x) => {
-			if(x.get('nightTime') == true) {
+			if(x.get('nightTime') === true) {
 				this.epipelagicCapOverlaySvg = '/public/assets/epipelagic-cap-overlay-night.svg';
 				this.epipelagicColor         = 'rgba(19,15,39,0.4)';
 			} else {
@@ -31,6 +34,17 @@ export class EpipelagicZoneComponent implements OnInit{
 			
 		});
 
+		this.zoom$.subscribe((x) => {
+			if(x.get('showHide') === false) {
+				this.overlayMargin = this.oceanCap.nativeElement.offsetHeight + 'px';
+			}
+		});
+
+	}
+
+	@HostListener('window:resize', ['$event'])
+	onResize(event) {
+		this.overlayMargin = this.oceanCap.nativeElement.offsetHeight + 'px';
 	}
 
 }
