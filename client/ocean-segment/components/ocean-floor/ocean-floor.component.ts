83bd3d6bd2e90b3965be8ada47c13a2cdf4532a3
Chris <chaugen24@knights.ucf.edu>
2017-01-11 19:12:38 -0800

    added ocean floor

--- client/ocean-segment/components/ocean-floor/ocean-floor.component.ts
+++ client/ocean-segment/components/ocean-floor/ocean-floor.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
+
+import { select } from 'ng2-redux';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'ocean-floor',
@@ -8,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
 
 export class OceanFloorComponent implements OnInit{ 
 
+	@ViewChild('floor') floor: ElementRef;
+	@select('zoom') zoom$: Observable<any>;
+
+	lockerMargin: number;
+
 	constructor() {}
 
 	ngOnInit() {
+		this.zoom$.subscribe((x) => {
+			if(x.get('showHide') === false) {
+				this.lockerMargin = this.floor.nativeElement.offsetHeight * 0.4;
+			}
+		});
 		
+	}
+
+	@HostListener('window:resize', ['$event'])
+	onResize(event) {
+		this.lockerMargin = this.floor.nativeElement.offsetHeight * 0.4;
 	}
 
 }
