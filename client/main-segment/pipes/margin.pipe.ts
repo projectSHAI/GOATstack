65c396676be1c955bb5f25cf8238759b34adaed9
Chris <chaugen24@knights.ucf.edu>
2017-01-16 21:27:33 -0800

    made layout responsive

--- client/main-segment/pipes/margin.pipe.ts
+++ client/main-segment/pipes/margin.pipe.ts
@@ -5,15 +5,15 @@ import { Observable } from 'rxjs/Observable';
 // created as a universal ngFor pipe so get the element when it's made 
 // can be used for animation or callbacks
 @Pipe({
-	name: 'marginPipe'
+	name: 'marginPipe',
+	pure: false
 })
 
 export class MarginPipe implements PipeTransform {
 
   transform(value: number, el: any): any {
-
+  	console.log('hello');
 		return el.offsetHeight;
-
   }
 
 }
