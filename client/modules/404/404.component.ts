a50f1526b2f7fbea00b573588997c8a3add2808a
Chris <chaugen24@knights.ucf.edu>
2017-01-23 21:32:41 -0800

    refactored 404 page

--- client/modules/404/404.component.ts
+++ client/modules/404/404.component.ts
@@ -2,8 +2,8 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'four0four-section',
-  template: '<p>404 Page not found</p>',
-  styles: ['p {font-size: 48px; display: block; position: relative; width: 100%; text-align: center; margin-top: 100px;}'],
+  templateUrl: './404.component.html',
+  styleUrls: ['./404.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
