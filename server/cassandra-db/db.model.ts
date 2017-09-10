a46ca4f7ba4fac242cdeca2fb0bd0ae747b8ff8c
Chris <chaugen24@knights.ucf.edu>
2017-09-10 16:58:27 -0700

    server(cassandra) seed function should return each cql query

--- server/cassandra-db/db.model.ts
+++ server/cassandra-db/db.model.ts
@@ -11,8 +11,8 @@ class DbModel {
 
     public seed(table: string, truncate: string, queries: Array<{ query: string, params: Array<any> }>, queryOptions: object): Promise<any> {
         return client.execute(table).then(result => {
-            client.execute(truncate).then(result => {
-                this.batch(queries, queryOptions).then(() => console.log('Batching succesful!')).catch(err => console.error(err));
+            return client.execute(truncate).then(result => {
+                return this.batch(queries, queryOptions).then(() => console.log('Batching succesful!')).catch(err => console.error(err));
             }).catch(err => console.error(err));
         }).catch(err => console.error(err));
     }
