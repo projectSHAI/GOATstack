87beba293653e12dffb2f1d96cea3bb4bdd9dc0d
Christopher Haugen <chaugen24@knights.ucf.edu>
2017-11-13 01:37:55 -0500

    client(HttpClient) upgrade deprecated Http to new HttpClient module

--- client/modules/core/core.module.ts
+++ client/modules/core/core.module.ts
@@ -1,9 +1,7 @@
 import { NgModule }                              from '@angular/core';
-import { HttpModule }                            from '@angular/http';
+import { HttpClientModule, HttpClient }          from '@angular/common/http';
 import { SharedModule }                          from '../shared/shared.module';
 import { CoreRoutingModule }                     from './core-routing.module';
-
-import { Http, XHRBackend, RequestOptions }      from '@angular/http';
 
 import { CoreComponent }		                     from './core.component';
 import { HeaderComponent }                       from './components/header/header.component';
@@ -21,9 +19,9 @@ let createLogger = require('redux-logger');
 @NgModule({
   imports:      [ SharedModule, CoreRoutingModule ],
   declarations: [ CoreComponent, HeaderComponent, FooterComponent ],
-  exports:      [ CoreRoutingModule, HttpModule, CoreComponent, HeaderComponent, FooterComponent ],
+  exports:      [ CoreRoutingModule, HttpClientModule, CoreComponent, HeaderComponent, FooterComponent ],
   providers: 	[
-    Http,
+    HttpClient,
   	ErrorHandlerActions,
   	SEOActions,
 
